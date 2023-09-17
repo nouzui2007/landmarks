d1c3140cee0a6f7bc67fec44a178d336279e2a07
Hotaka Makiuchi <makiuchi@ccube.jp>
2023-09-17 15:22:17 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 牧内 on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
