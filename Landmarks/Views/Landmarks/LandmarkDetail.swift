98f5a6bba886c695df335ad215b75fd77650b9e5
Hotaka Makiuchi <makiuchi@ccube.jp>
2023-09-18 14:19:52 +0900

    Done Handling User Input

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -19,17 +24,22 @@ struct LandmarkDetail: View {
             CircleImage(image: landmark.image)
                 .offset(y: -100)
                 .padding(.bottom, -130)
-
+            
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
+                
                 HStack {
                     Text(landmark.park)
                     Spacer()
                     Text(landmark.state)
                 }
                 .font(.subheadline)
-
+                
                 Divider()
                 
                 Text("About \(landmark.name)")
@@ -46,7 +56,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[1])
+            .environmentObject(modelData)
     }
 }
---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 牧内 on 2023/09/18.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoriteOnly = true
+    
+    var filterdLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filterdLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
